6a737df8934d9db8a43459d3461140853b127b13
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-07-25 18:56:47 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,95 @@
-Set-Alias np "C:\Program Files\Notepad++\notepad++.exe"
+# Aliases
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Set-Alias -Name cat -Value bat
+Set-Alias -Name df -Value Get-Volume
+Set-Alias -Name ff -Value Find-File
 Set-Alias grep rg
-Set-Alias cat bat
-Set-Alias neofetch neofetch.exe
-Set-Alias reboot Restart-computer
-Set-Alias shutdown Stop-computer
-Set-Alias ip ipconfig
-oh-my-posh init pwsh --config 'C:\Users\lain\AppData\Local\Programs\oh-my-posh\themes\catppuccin_mocha.omp.json' | Invoke-Expression
-function ipp { (Invoke-WebRequest http://ifconfig.me/ip).Content }
+Set-Alias -Name l -Value Get-ChildItemPretty
+Set-Alias -Name la -Value Get-ChildItemPretty
+Set-Alias -Name ll -Value Get-ChildItemPretty
+Set-Alias -Name ls -Value Get-ChildItemPretty
+Set-Alias -Name rm -Value Remove-ItemExtended
+Set-Alias -Name su -Value sudo
+Set-Alias -Name tif Show-ThisIsFine
+Set-Alias -Name touch -Value New-File
+Set-Alias -Name us -Value Update-Software
+Set-Alias -Name v -Value nvim
+Set-Alias -Name vim -Value nvim
+Set-Alias np "C:\Program Files\Notepad++\notepad++.exe"
+Set-Alias drive Get-PSDrive
 
-
-function uptime {
-    if ($PSVersionTable.PSVersion.Major -eq 5) {
-        Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
-    } else {
-        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
-    }
+#Functions
+function Update-Software {
+    Write-Verbose "Updating software installed via Winget & Chocolatey"
+    sudo cache on
+    sudo winget upgrade --all --include-unknown --silent --verbose
+    sudo choco upgrade all -y
+    sudo -k
+    $ENV:SOFTWARE_UPDATE_AVAILABLE = ""
 }
 
-function df {
-    get-volume
+function Find-File {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 0)]
+        [string]$SearchTerm
+    )
+
+    Write-Verbose "Searching for '$SearchTerm' in current directory and subdirectories"
+    $result = Get-ChildItem -Recurse -Filter "*$SearchTerm*" -ErrorAction SilentlyContinue
+
+    Write-Verbose "Outputting results to table"
+    $result | Format-Table -AutoSize
 }
 
-function sysinfo { Get-ComputerInfo }
+function New-File {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Name
+    )
+
+    Write-Verbose "Creating new file '$Name'"
+    New-Item -ItemType File -Name $Name -Path $PWD | Out-Null
+}
+
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function Get-ChildItemPretty {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$Path = $PWD
+    )
+
+    Write-Host ""
+    eza -a -l --header --icons --hyperlink --time-style relative $Path
+    Write-Host ""
+}
+
+function Show-ThisIsFine {
+    Write-Verbose "Running thisisfine.ps1"
+    Show-ColorScript -Name thisisfine
+}
+
+function Remove-ItemExtended {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [switch]$rf,
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Path
+    )
+
+    Write-Verbose "Removing item '$Path' $($rf ? 'and all its children' : '')"
+    Remove-Item $Path -Recurse:$rf -Force:$rf
+}
+
+function winutil {
+	iwr -useb https://christitus.com/win | iex
+}
 
 function admin {
     if ($args.Count -gt 0) {
@@ -32,68 +100,31 @@ function admin {
     }
 }
 
-Set-Alias -Name su -Value admin
-
-function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
-function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
-
-function unzip ($file) {
-    Write-Output("Extracting", $file, "to", $pwd)
-    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
-    Expand-Archive -Path $fullFile -DestinationPath $pwd
-}
-
-function touch($file) { "" | Out-File $file -Encoding ASCII }
-function ff($name) {
-    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-        Write-Output "$($_.FullName)"
+function uptime {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+    } else {
+        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
     }
 }
 
-# Quick File Creation
-function nf { param($name) New-Item -ItemType "file" -Path . -Name $name }
+# Environment Variables
+$ENV:STARSHIP_CONFIG = "$ENV:WindotsLocalRepo\starship\starship.toml"
+$ENV:BAT_CONFIG_DIR = "$ENV:WindotsLocalRepo\bat"
+$ENV:FZF_DEFAULT_OPTS = '--color=fg:-1,fg+:#ffffff,bg:-1,bg+:#3c4048 --color=hl:#5ea1ff,hl+:#5ef1ff,info:#ffbd5e,marker:#5eff6c --color=prompt:#ff5ef1,spinner:#bd5eff,pointer:#ff5ea0,header:#5eff6c --color=gutter:-1,border:#3c4048,scrollbar:#7b8496,label:#7b8496 --color=query:#ffffff --border="rounded" --border-label="" --preview-window="border-rounded" --height 40% --preview="bat -n --color=always {}"'
 
-# Directory Management
-function mkcd { param($dir) mkdir $dir -Force; Set-Location $dir }
+# Prompt Setup
+Invoke-Expression (&starship init powershell)
+Enable-TransientPrompt
+Invoke-Expression (& { ( zoxide init powershell --cmd cd | Out-String ) })
 
-function doc { Set-Location -Path $HOME\Documents }
 
-function pic { Set-Location -Path $HOME\Pictures }
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineKeyHandler -Function AcceptSuggestion -Key Alt+l
+Import-Module -Name CompletionPredictor
 
-function desk { Set-Location -Path $HOME\Desktop }
-
-function down { Set-Location -Path $HOME\Downloads }
-
-function help {
-    @"
-PowerShell Profile Help
-=======================
-• ipp - Retrieves the public IP address of the machine.
-
-• uptime - Displays the system uptime.
-
-• unzip <file> - Extracts a zip file to the current directory.
-
-• grep <regex> [dir] - Searches for a regex pattern in files within the specified directory or from the pipeline input.
-
-• df - Displays information about volumes.
-
-• touch <file> - Creates a new empty file.
-
-• nf <name> - Creates a new file with the specified name.
-
-• mkcd <dir> - Creates and changes to a new directory.
-
-• docs - Changes the current directory to the user's Documents folder.
-
-• desk - Changes the current directory to the user's Desktop folder.
-
-• la - Lists all files in the current directory with detailed formatting.
-
-• sysinfo - Displays detailed system information.
-
-• ll - Lists all files, including hidden, in the current directory with detailed formatting.
-"@
+if ([Environment]::GetCommandLineArgs().Contains("-NonInteractive") -or [Environment]::GetCommandLineArgs().Contains("-CustomPipeName")) {
+    return
 }
-
-Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+fastfetch
